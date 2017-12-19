b3c9fcb8f3f74973090b1d02a508fc89bc204669
wesias7 <wesias7@gmail.com>
2017-12-19 16:02:38 +0900

    a
    a

--- src/index.js
+++ src/index.js
@@ -8,4 +8,5 @@ document.cookie = 'key_a=1;path=/';
 document.cookie = 'key_b=2;path=/';
 const cookie = document.cookie;
 document.write(cookie);
+
 //document.write(`<div>Webpack Hot Module Replace Plugin with Webpack Development Server within ExpressJs Middleware.${JSON.stringify(xml_loader)}</div>`);
